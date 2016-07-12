e1fff97719c39a4431cddc3e3fec1e0411a944fb
brock <brock@supermarly.com>
2016-07-12 17:17:37 +0800

    iptables

--- scripts/linux_server.sh
+++ scripts/linux_server.sh
@@ -4,5 +4,6 @@ net=$2
 dev=$3
 ifconfig ${ifn} ${net}.1 pointopoint ${net}.2 mtu 1400 up
 echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -D POSTROUTING -s ${net}.2/32 -o ${dev} -j MASQUERADE
 iptables -t nat -A POSTROUTING -s ${net}.2/32 -o ${dev} -j MASQUERADE
 echo $*
